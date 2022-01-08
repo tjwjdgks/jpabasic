f24297e985a60f9151ade359a86e248c0a7796ed
tjwjdgks <tjwjdgks43@gmail.com>
2022-01-08 21:08:39 +0900

    jpabook v6

---
+++ src/main/java/jpabook/domain/Address.java
@@ -0,0 +1,54 @@
+package jpabook.domain;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
+import java.util.Objects;
+
+// 비즈니스 로직을 만들 수 있다
+@Embeddable
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    private void setCity(String city) {
+        this.city = city;
+    }
+
+    private void setStreet(String street) {
+        this.street = street;
+    }
+
+    private void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    // proxy를 고려해서 항상 메서드를 통해서 값을 불러올 수 있도록 해야 한다
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(getCity(), address.getCity())
+                && Objects.equals(getStreet(), address.getStreet())
+                && Objects.equals(getZipcode(), address.getZipcode());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCity(), getStreet(), getZipcode());
+    }
+}
